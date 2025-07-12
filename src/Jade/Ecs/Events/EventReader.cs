1d52619fff8c69e3cf07132f38066e2e9f6fd632
jimmy-mll <jimmy.mll@hotmail.com>
2025-07-12 20:48:02 +0200

    feat: add EventReader and EventWriter for event handling in ECS

---
+++ src/Jade/Ecs/Events/EventReader.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) AerafalGit 2025.
+// Jade licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Jade/blob/main/LICENSE.
+
+using System.Collections;
+
+namespace Jade.Ecs.Events;
+
+public sealed class EventReader<T> : IEnumerable<T>
+    where T : unmanaged, IEvent
+{
+    private readonly EventBus _eventBus;
+
+    internal EventReader(EventBus eventBus)
+    {
+        _eventBus = eventBus;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return _eventBus.Read<T>().GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
