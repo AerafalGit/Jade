a291f3fed59b94e852ddfc7593d47e98933bf99e
jimmy-mll <jimmy.mll@hotmail.com>
2025-07-12 19:29:08 +0200

    feat: add Assets class for managing and retrieving typed assets

---
+++ src/Jade/Assets/Assets.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) AerafalGit 2025.
+// Jade licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Jade/blob/main/LICENSE.
+
+namespace Jade.Assets;
+
+public sealed class Assets<T>
+    where T : class
+{
+    public Handle<T> GetOrAdd(T asset)
+    {
+        return new Handle<T>(0);
+    }
+
+    public T? Get(Handle<T> handle)
+    {
+        return null;
+    }
+}
