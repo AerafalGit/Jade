39a0f6cbf13cf8ba8f9b556e819bab3bcc973a86
jimmy-mll <jimmy.mll@hotmail.com>
2025-07-12 15:54:18 +0200

    feat: implement Archetype system with Archetype, ArchetypeChunk, and ArchetypeGraph

---
+++ src/Jade/Ecs/Archetypes/Archetype.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) AerafalGit 2025.
+// Jade licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Jade/blob/main/LICENSE.
+
+using Jade.Ecs.Abstractions;
+using Jade.Ecs.Components;
+
+namespace Jade.Ecs.Archetypes;
+
+internal sealed class Archetype : IDisposable
+{
+    public ComponentMask Mask { get; }
+
+    public List<ArchetypeChunk> Chunks { get; }
+
+    public int EntityCount { get; private set; }
+
+    public ComponentId[] ComponentIds { get; }
+
+    public Dictionary<ComponentId, Archetype> AddTransitions { get; }
+
+    public Dictionary<ComponentId, Archetype> RemoveTransitions { get; }
+
+    public Archetype(ComponentMask mask)
+    {
+        Mask = mask;
+        ComponentIds = Mask.GetComponents();
+        Chunks = [new ArchetypeChunk(ComponentIds)];
+        AddTransitions = [];
+        RemoveTransitions = [];
+    }
+
+    ~Archetype()
+    {
+        ReleaseUnmanagedResources();
+    }
+
+    public (int chunkIndex, int indexInChunk) Add(Entity entity)
+    {
+        var chunk = GetChunkForAdd();
+        var indexInChunk = chunk.Add(entity);
+        EntityCount++;
+        return (Chunks.Count - 1, indexInChunk);
+    }
+
+    public Entity Remove(int chunkIndex, int indexInChunk)
+    {
+        var chunk = Chunks[chunkIndex];
+        var movedEntity = chunk.Remove(indexInChunk);
+        EntityCount--;
+        return movedEntity;
+    }
+
+    private ArchetypeChunk GetChunkForAdd()
+    {
+        var lastChunk = Chunks[^1];
+        if (lastChunk.IsFull)
+        {
+            lastChunk = new ArchetypeChunk(ComponentIds);
+            Chunks.Add(lastChunk);
+        }
+        return lastChunk;
+    }
+
+    private void ReleaseUnmanagedResources()
+    {
+        foreach (var chunk in Chunks)
+            chunk.Dispose();
+    }
+
+    public void Dispose()
+    {
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ src/Jade/Ecs/Archetypes/ArchetypeGraph.cs
@@ -0,0 +1,116 @@
+﻿// Copyright (c) AerafalGit 2025.
+// Jade licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Jade/blob/main/LICENSE.
+
+using Jade.Ecs.Components;
+using Jade.Ecs.Queries;
+
+namespace Jade.Ecs.Archetypes;
+
+internal sealed class ArchetypeGraph : IDisposable
+{
+    private readonly Dictionary<ComponentMask, Archetype> _archetypes;
+
+    public Archetype Root { get; }
+
+    public QueryCache QueryCache { get; }
+
+    public ArchetypeGraph()
+    {
+        _archetypes = [];
+
+        Root = GetOrCreateArchetype(new ComponentMask());
+        QueryCache = new QueryCache(this);
+    }
+
+    ~ArchetypeGraph()
+    {
+        ReleaseUnmanagedResources();
+    }
+
+    public Archetype GetOrCreateArchetype(in ComponentMask mask)
+    {
+        if (_archetypes.TryGetValue(mask, out var archetype))
+            return archetype;
+
+        var newArchetype = new Archetype(mask);
+        BuildEdges(newArchetype);
+        _archetypes[mask] = newArchetype;
+
+        QueryCache.InvalidateCache();
+
+        return newArchetype;
+    }
+
+    public IEnumerable<Archetype> GetArchetypesWith(ComponentMask mask)
+    {
+        return _archetypes.Values.Where(archetype => archetype.Mask.HasAll(mask));
+    }
+
+    public IEnumerable<Archetype> GetArchetypesWith(ComponentMask allMask, ComponentMask anyMask, ComponentMask noneMask)
+    {
+        foreach (var (mask, archetype) in _archetypes)
+        {
+            if (!allMask.IsEmpty && !mask.HasAll(allMask))
+                continue;
+
+            if (!anyMask.IsEmpty && !mask.HasAny(anyMask))
+                continue;
+
+            if (!noneMask.IsEmpty && mask.HasAny(noneMask))
+                continue;
+
+            yield return archetype;
+        }
+    }
+
+    private void BuildEdges(in Archetype newArchetype)
+    {
+        foreach (var existingArchetype in _archetypes.Values)
+        {
+            if (existingArchetype == newArchetype)
+                continue;
+
+            TryCreateEdge(newArchetype, existingArchetype);
+            TryCreateEdge(existingArchetype, newArchetype);
+        }
+    }
+
+    private static void TryCreateEdge(in Archetype from, in Archetype to)
+    {
+        var fromMask = from.Mask;
+        var toMask = to.Mask;
+
+        if (!toMask.HasAll(in fromMask))
+            return;
+
+        var diffMask = toMask & ~fromMask;
+
+        var popCount = diffMask.PopCount();
+
+        if (popCount is 1)
+        {
+            var componentId = diffMask.FirstSetBit();
+
+            if (!componentId.IsRelation)
+            {
+                from.AddTransitions[componentId] = to;
+                to.RemoveTransitions[componentId] = from;
+            }
+        }
+    }
+
+    private void ReleaseUnmanagedResources()
+    {
+        Root.Dispose();
+
+        foreach (var archetype in _archetypes.Values)
+            archetype.Dispose();
+    }
+
+    public void Dispose()
+    {
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+}
