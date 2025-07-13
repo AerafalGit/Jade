08b62fec32fe7b91b98edd96df84daed230c983a
jimmy-mll <jimmy.mll@hotmail.com>
2025-07-13 02:55:41 +0200

    feat: add Query API for streamlined ECS queries in World class

---
+++ src/Jade/Ecs/World.Queries.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) AerafalGit 2025.
+// Jade licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Jade/blob/main/LICENSE.
+
+using System.Runtime.CompilerServices;
+using Jade.Ecs.Queries;
+
+namespace Jade.Ecs;
+
+public sealed partial class World
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Query Query()
+    {
+        return new Query(this);
+    }
+}
