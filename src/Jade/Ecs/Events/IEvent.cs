317aba3f282433a1b212fce479b1f525ef452e3d
jimmy-mll <jimmy.mll@hotmail.com>
2025-07-16 17:08:19 +0200

    chore: global clean up

--- src/Jade/Ecs/Events/IEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) AerafalGit 2025.
-// Jade licenses this file to you under the MIT license.
-// See the license here https://github.com/AerafalGit/Jade/blob/main/LICENSE.
-
-namespace Jade.Ecs.Events;
-
-public interface IEvent;
