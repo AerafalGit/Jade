aa0347f7bed595d8b7ff0ce8f459366769215de9
jimmy-mll <jimmy.mll@hotmail.com>
2025-07-16 18:55:50 +0200

    feat: add ComponentArray base and specialized classes for ECS component management

---
+++ src/Jade/Ecs/Components/ComponentArray.Unmanaged.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) AerafalGit 2025.
+// Jade licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Jade/blob/main/LICENSE.
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Jade.Ecs.Components;
+
+public sealed unsafe class ComponentArrayUnmanaged<T> : ComponentArray<T>
+{
+    public T* Ptr { get; }
+
+    public int ComponentSize { get; }
+
+    public override int Capacity { get; }
+
+    public ComponentArrayUnmanaged(int size, int alignment, int capacity)
+    {
+        Capacity = capacity;
+        ComponentSize = size;
+        Ptr = (T*)NativeMemory.AlignedAlloc((nuint)(ComponentSize * Capacity), (nuint)alignment);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override Span<T> GetSpan(int count)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(count);
+        return new Span<T>(Ptr, Math.Min(count, Capacity));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override ref T GetRef(int index)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(index);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, Capacity);
+        return ref Ptr[index];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override void SetRef(int index, in T value)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(index);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, Capacity);
+        Ptr[index] = value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override void Move(int fromIndex, int toIndex)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(fromIndex);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(fromIndex, Capacity);
+        ArgumentOutOfRangeException.ThrowIfNegative(toIndex);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(toIndex, Capacity);
+
+        if (fromIndex == toIndex)
+            return;
+
+        Ptr[toIndex] = Ptr[fromIndex];
+        Ptr[fromIndex] = default;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override void Reset()
+    {
+        NativeMemory.Clear(Ptr, (nuint)(ComponentSize * Capacity));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    protected override void ReleaseUnmanagedResources()
+    {
+        NativeMemory.Free(Ptr);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    protected override void MoveTo(int fromIndex, ComponentArray<T> toArray, int toIndex)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(fromIndex);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(fromIndex, Capacity);
+        ArgumentOutOfRangeException.ThrowIfNegative(toIndex);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(toIndex, toArray.Capacity);
+
+        toArray.SetRef(toIndex, GetRef(fromIndex));
+        Ptr[fromIndex] = default;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    protected override void CopyTo(int fromIndex, ComponentArray<T> toArray, int toIndex)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(fromIndex);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(fromIndex, Capacity);
+        ArgumentOutOfRangeException.ThrowIfNegative(toIndex);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(toIndex, toArray.Capacity);
+
+        switch (toArray)
+        {
+            case ComponentArrayUnmanaged<T> unmanagedArray:
+                var source = (byte*)Ptr + fromIndex * ComponentSize;
+                var dest = (byte*)unmanagedArray.Ptr + toIndex * ComponentSize;
+                NativeMemory.Copy(source, dest, (nuint)ComponentSize);
+                break;
+
+            case ComponentArrayManaged<T> managedArray:
+                var sourceSpan = MemoryMarshal.CreateSpan(ref GetRef(fromIndex), 1);
+                var destSpan = MemoryMarshal.CreateSpan(ref managedArray.GetRef(toIndex), 1);
+                sourceSpan.CopyTo(destSpan);
+                break;
+
+            default:
+                toArray.SetRef(toIndex, GetRef(fromIndex));
+                break;
+        }
+    }
+}
